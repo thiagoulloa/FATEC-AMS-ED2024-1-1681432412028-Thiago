25087f7d97cc0c0d2387e771b8b30b757c2f9a5e
Thiago Ulloa <84195990+thiagoulloa@users.noreply.github.com>
2024-03-19 15:14:34 -0300

    Cabeçalho

--- Atividade B1-3/programa1.c
+++ Atividade B1-3/programa1.c
@@ -1,3 +1,12 @@
+/*--------------------------------------------------------------------------*/
+/*   FATEC-São Caetano do Sul                 Estrutura de Dados            */
+/*                         Prof Veríssimo                                   */
+/*             Objetivo: Exercícios envolvendo ponteiros e arrays           */
+/*             Paradigama: Programação Modular                              */
+/*                                                                          */
+/*                                                       Data:18/03/2024    */
+/*--------------------------------------------------------------------------*/
+
 #include <stdio.h>
 
 int encontrar_maior_elemento(int *array, int tamanho) {
